94cdf58da3cd4896aecc2dd48a7c1e6318626126
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2014-10-13 20:57:18 +0200

    Task 'kopirka' improved a bit

--- zadace/dz01/kopirka.cpp
+++ zadace/dz01/kopirka.cpp
@@ -4,32 +4,27 @@
 
 using namespace std;
 
+typedef long long lld;
+
 int a[505];
 vector<int> sol;
-
-long long max(long long a, long long b)
-{
-    if(a > b)
-        return a;
-    return b;
-}
 
 int main()
 {
     int M, K;
-    long long lo = 0, hi = 0, mid;
+    lld lo = 0, hi = 0, mid;
     scanf("%d%d", &M, &K);
     for(int i = 0; i < M; i++)
     {
         scanf("%d", a + i);
         hi += a[i];
-        lo = max(lo, a[i]);
+        lo = max<lld>(lo, a[i]);
     }
 
     while(lo < hi)
     {
         mid = (hi + lo) / 2;
-        long long sum = 0;
+        lld sum = 0;
         int p = 1;
         for(int i = 0; i < M; i++)
         {
@@ -47,7 +42,7 @@ int main()
             lo = mid + 1;
     }
 
-    long long sum = 0;
+    lld sum = 0;
     int p = K - 1;
     for(int i = M - 1; i >= 0; i--)
     {
@@ -61,12 +56,14 @@ int main()
         sol.push_back(a[i]);
         sum += a[i];
     }
+
     reverse(sol.begin(), sol.end());
     int len = sol.size();
     for(int i = 0; i < len; i++)
         if(sol[i] == -1)
             printf("/ ");
-        else {
+        else
+        {
             printf("%d", sol[i]);
             if(i != len - 1)
                 printf(" ");
