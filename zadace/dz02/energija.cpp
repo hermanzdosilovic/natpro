08efe241fc5cf1ab3ec42e5de5b9fa442c3b7d54
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2014-10-23 00:23:29 +0200

    Solved 60% energija

---
+++ zadace/dz02/energija.cpp
@@ -0,0 +1,97 @@
+#include <cstdio>
+#include <algorithm>
+#define x first
+#define y second
+#define mp make_pair
+
+using namespace std;
+
+typedef long long lld;
+typedef pair<lld, lld> llp;
+
+const lld INF = 0x7fffffffffffffffL;
+const int MAXL = 100000;
+
+llp tree[1 << 18];
+bool sensor[MAXL + 3];
+int I, L;
+lld R, K;
+char c[6];
+
+void update(int n, int l, int r) {
+  if (r < I || l > I) {
+    return;
+  } else if (l == r && r == I) {
+    tree[n] = mp(K, K);
+    if(!sensor[I]) {
+      tree[n] = mp(0, INF);
+    }
+    return;
+  } else if (l == r && r != I) {
+    return;
+  }
+  update(2*n, l, (l+r)/2);
+  update(2*n + 1, (l+r)/2 + 1, r);
+  tree[n].x = max(tree[2*n].x, tree[2*n + 1].x);
+  tree[n].y = min(tree[2*n].y, tree[2*n + 1].y);
+}
+
+llp query(int n, int l, int r) {
+  if (l > I) {
+    return mp(0, INF);
+  }
+  else if (l >= 1 && r <= I) {
+    return tree[n];
+  }
+  llp q = query(2*n, l, (l+r)/2);
+  llp p = query(2*n + 1, (l+r)/2 + 1, r);
+  return mp(max(q.x, p.x), min(q.y, p.y));
+}
+
+void init(int n, int l, int r) {
+  tree[n] = mp(0, INF);
+  if(l == r) {
+    return;
+  }
+  init(2*n, l, (l+r)/2);
+  init(2*n + 1, (l+r)/2 + 1, r);
+}
+
+int main() {
+  scanf("%d", &L);
+  init(1, 1, L - 1);
+  while (1) {
+    scanf("%s", c);
+    if (c[0] == 'A') {
+      scanf("%d%lld", &I, &K);
+      I++;
+      sensor[I] = true;
+      update(1, 1, L - 1);
+    } else if (c[0] == 'D') {
+      scanf("%d", &I);
+      I++;
+      sensor[I] = false;
+      K = 0;
+      update(1, 1, L - 1);
+    } else if (c[0] == 'Q') {
+      scanf("%lld", &R);
+      int sol = 0;
+      lld najveca = 0;
+      for (int i = 0; i < L; i++) {
+        if (!sensor[i + 1])
+          continue;
+        I = i + 1;
+        llp n = query(1, 1, L - 1);
+        lld trenutna = (n.x - n.y)*i;
+        if(trenutna <= R && trenutna >= najveca) {
+          najveca = trenutna;
+          sol = i;
+        }
+      }
+      printf("%d\n", sol);
+    } else {
+      break;
+    }
+  }
+  return 0;
+}
