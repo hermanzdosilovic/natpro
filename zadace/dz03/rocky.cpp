f4fc286cb4ab1c9753f537ef21ecaa7832dd5a78
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2014-10-27 00:38:12 +0100

    Solved task rocky

--- zadace/dz03/rocky.cpp
+++ zadace/dz03/rocky.cpp
@@ -4,21 +4,16 @@
 const int MOD = 1000007;
 const int MAXN = 1000000;
 
-int memo[MAXN + 3];
-
-int f(int N) {
-  if (memo[N] != -1)
-    return memo[N];
-  memo[N] = (f(N - 1)%MOD + f(N - 2)%MOD + f(N - 3)%MOD)%MOD;
-  return memo[N];
-}
-
+int dp[MAXN + 3];
 int main() {
   int N;
   scanf("%d", &N);
-  memset(memo, -1, sizeof(memo));
-  memo[0] = memo[1] = 1;
-  memo[2] = 2;
-  printf("%d\n", f(N));
+  dp[0] = dp[1] = 1;
+  dp[2] = 2;
+  for (int i = 3; i <= N; i++) {
+    dp[i] = dp[i - 1]%MOD + dp[i - 2]%MOD + dp[i - 3]%MOD;
+    dp[i] %= MOD;
+  }
+  printf("%d\n", dp[N]);
   return 0;
 }
